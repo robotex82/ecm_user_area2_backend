4b773d30bc082fe9b35bc23f991fda40db62eceb
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2018-06-19 21:38:39 +0200

    Fix routes for services.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Ecm::UserArea::Backend::Engine.routes.draw do
-  resources :create_default_user_service, only: [:index] do
-    get  :invoke, on: :collection
-    post :call,   on: :collection
-  end
+  resources :create_default_user_service, only: [:new, :create]
   
   backend_resources :users do
     get :autocomplete, on: :collection
